0566c846221dbe7814a3b6a48839e83ce5c17470
Artur <>
2024-04-27 16:35:44 +0300

    создали заготовку

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const categoriesRouter = require("./categories");
+const gamesRouter = require("./games");
+const usersRouter = require("./users");
+
+module.exports = {
+  gamesRouter,
+  usersRouter,
+  categoriesRouter,
+};
