053f520759cfd6df758c823da1414d5ca6501c80
Artur <>
2024-05-02 20:59:38 +0300

    CRUD and Check

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,40 @@
 const categoriesRouter = require("express").Router();
 
-const findAllCategories = require("../middlewares/categories");
-const sendAllCategories = require("../controllers/categories");
+const {
+  findAllCategories,
+  createCategory,
+  findCategoryById,
+  updateCategory,
+  deleteCategory,
+  checkIsCategoryExists,
+  checkEmptyName,
+} = require("../middlewares/categories");
+const {
+  sendAllCategories,
+  sendCategoryCreated,
+  sendCategoryById,
+  sendCategoryUpdated,
+  sendCategoryDeleted,
+} = require("../controllers/categories");
 
 categoriesRouter.get("/categories", findAllCategories, sendAllCategories);
+categoriesRouter.get("/categories/:id", findCategoryById, sendCategoryById);
+
+categoriesRouter.post(
+  "/categories",
+  findAllCategories,
+  checkIsCategoryExists,
+  checkEmptyName,
+  createCategory,
+  sendCategoryCreated
+);
+categoriesRouter.put(
+  "/categories/:id",
+  checkEmptyName,
+  updateCategory,
+  sendCategoryUpdated
+);
+
+categoriesRouter.delete("/categories/:id", deleteCategory, sendCategoryDeleted);
 
 module.exports = categoriesRouter;
