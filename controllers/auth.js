4d5acd214fd10318d948bb074ea442ced32a1dd6
Artur <>
2024-05-11 00:15:40 +0300

    Добавили статические методы, декомпозировали код

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,19 +5,7 @@ const login = (req, res) => {
   const { email, password } = req.body;
 
   users
-    .findOne({ email })
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error("Неправильные почта или пароль"));
-      }
-
-      return bcrypt.compare(password, user.password).then((matched) => {
-        if (!matched) {
-          return Promise.reject(new Error("Неправильные почта или пароль"));
-        }
-        return user;
-      });
-    })
+    .findUserByCredentials(email, password)
     .then((user) => {
       res
         .status(200)
@@ -27,5 +15,4 @@ const login = (req, res) => {
       res.status(401).send({ message: error.message });
     });
 };
-
 module.exports = { login };
